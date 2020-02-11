2feee5d0459ffa42f6064cfa921248a427babbc1
Caitlin <cmoconnor0823@gmail.com>
2020-02-11 11:18:04 -0700

    end of oop and beginning of harmless note

---
+++ 99-Additional_Pratice/harmlessNote.js
@@ -0,0 +1,9 @@
+/*Harmless Ransom Note
+A note made out of words cut from a magazine (object in this case)
+This function will take in 2 inputs, the string of the note we want to write
+the second will be the magazine text we have to use
+The function will look at the note we want to write, and then check the 
+Magazine to see if it contains each word we need to use
+ex:  if we need a word more than once we should find one word 
+to cut out for each time we need it */
+
